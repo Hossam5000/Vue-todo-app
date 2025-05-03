2871e4fdf51d57cc9058cbdcfaaf9be5d312e817
Hossam <84795275+Hossam5000@users.noreply.github.com>
2025-05-03 22:11:02 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+// imports
+import { ref } from 'vue';
+import todoItem from './components/todoItem.vue'
+
+// cons & vars
+const inputNewTodo = ref("");
+
+// functions
+
+</script>
+
+<template>
+  new app
+  <div class="head-container">
+    <input v-model="inputNewTodo" placeholder="Add new todo">
+    <button>ADD</button>
+  </div><!--./container-->
+
+  <!-- todo list -->
+  <ul class="todoList">
+    <todoItem />
+  </ul>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.head-container {
+  background: purple;
+  padding: 3px;
+}
+
+ul {
+  border: 1px solid #000;
+}
+
+ul li {
+  list-style-type: none;
+}
+</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <li class="todoItem">
+
+    <div class="left">
+      <i class="fa-regular fa-circle"></i>
+      <span>test task</span>
+    </div><!--./left-->
+
+    <div class="right">
+      <span class="delete-btn"><i class="fa-solid fa-trash"></i></span>
+    </div><!--./right-->
+  </li>
+</template>
+
+<style scoped>
+  li.todoItem{
+    display: flex;
+    justify-content: space-between;
+    background-color: dodgerblue;
+    color: #000;
+  }
+</style>
