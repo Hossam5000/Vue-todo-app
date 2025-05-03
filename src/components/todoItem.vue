14dfe8630540c3e3cd812c02fa48779040aa978f
Hossam <84795275+Hossam5000@users.noreply.github.com>
2025-05-03 19:24:47 +0000

    new

--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,25 +1,25 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <li class="todoItem">
-
     <div class="left">
       <i class="fa-regular fa-circle"></i>
       <span>test task</span>
-    </div><!--./left-->
+    </div>
+    <!--./left-->
 
     <div class="right">
       <span class="delete-btn"><i class="fa-solid fa-trash"></i></span>
-    </div><!--./right-->
+    </div>
+    <!--./right-->
   </li>
 </template>
 
 <style scoped>
-  li.todoItem{
-    display: flex;
-    justify-content: space-between;
-    background-color: dodgerblue;
-    color: #000;
-  }
+li.todoItem {
+  display: flex;
+  justify-content: space-between;
+  background-color: green;
+  color: #000;
+}
 </style>
