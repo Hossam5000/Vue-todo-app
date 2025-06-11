e5a0782c2599f4aa2774905eb5990518edac7e66
Hossam <codingWorld5000@gmail.com>
2025-06-11 21:14:35 +0300

    starting again the vue.js course

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,17 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import todoItem from "./components/todoItem.vue";
 
-const props = defineProps(["taskTitle",]);
-const inputNewTodo = ref("");
-let todos = ref([]);
-
-// functions
-function addTodo() {
-  if (!inputNewTodo.value.trim()) return; // Prevent empty todos
-
-  const todo = {
-    id: Date.now(), // Unique identifier
-    text: inputNewTodo.value,
-    completed: false,
-    editing: false, // Track edit mode
-  };
-
-  todos.value.push(todo);
-  save();
-  inputNewTodo.value = "";
-}
-
-function editTodo() {
-  console.log("edit");
-}
-
-function save() {
-  localStorage.setItem("todos", JSON.stringify(todos.value));
-}
-
-function load() {
-  todos.value = JSON.parse(localStorage.getItem("todos") || "[]");
-}
-
-function deleteTodo(index) {
-  todos.value.splice(index, 1);
-  save();
-}
-
-onMounted(() => {
-  load();
-});
 </script>
 
 <template>
-  <h2 class="title">TODO APP</h2>
-  <div class="app-container">
-    <div class="head-container">
-      <input @keyup.enter="addTodo" v-model="inputNewTodo" placeholder="Add new todo" />
-      <button @click="addTodo">ADD</button>
-    </div>
-    <!-- ./head-container -->
-
-    <!-- todo list -->
-    <ul class="todoList">
-      <todoItem v-for="todo in todos" :key="todo.id" :task-title="todo.text" @delete="deleteTodo(todos.indexOf(todo))"
-        @edit="editTodo" />
-    </ul>
-  </div>
-  <!-- ./app-container -->
+  <main class="main">
+    the parent element
+    <todoItem />
+  </main>
 </template>
 
 <style scoped>
-/* App.css */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-h2 {
-  margin: 30px auto 10px;
-  text-align: center;
-}
-
-.head-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid #000;
-  border-radius: 30px;
-  padding: 3px;
-}
-
-.head-container input {
-  width: 100%;
-  padding-left: 10px;
-  background: none;
-}
-
-.head-container button {
-  padding: 10px 30px;
-  border-radius: 30px;
-  background: #ff0066;
-  color: #fff;
-  cursor: pointer;
-}
-
-.head-container input,
-.head-container button {
-  outline: none;
-  border: none;
-}
-
-ul.todoList {
-  margin-top: 30px;
-  padding: 0 10px;
-}
-
-ul.todo-list li {
-  list-style-type: none;
+.main {
+  background-color: green;
 }
 </style>
--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,73 +1,13 @@
 <script setup>
-// cons & vars
 
-const emits = defineEmits(["delete", "edit"]);
 </script>
 
 <template>
-  <li class="todoItem">
-
-    <div class="left">
-      <i class="fa-regular fa-circle"></i>
-      <input :value="taskTitle" />
-    </div>
-    <!-- ./left -->
-
-    <div class="right">
-      <span @click="$emit('edit')" class="edit-btn">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </span>
-
-      <span @click="$emit('delete')" class="delete-btn">
-        <i class="fa-solid fa-trash"></i>
-      </span>
-    </div>
-    <!-- ./right -->
-  </li>
+  <h1>just a test for a headline 1</h1>
 </template>
 
 <style scoped>
-/* todoItem.css */
-li.todoItem {
-  display: flex;
-  justify-content: space-between;
-  margin: 10px 0;
-  padding: 10px 5px;
-  border-radius: 5px;
-  border: 1px solid #ff0066;
-  color: #000;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-}
-
-li.todoItem:hover {
-  border-color: purple;
-}
-
-.left i {
-  margin-right: 5px;
-  font-size: 20px;
-}
-
-.left {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.right span i {
-  color: #000;
-}
-
-.right .delete-btn {
-  margin-left: 20px;
-}
-
-.right .delete-btn i:hover {
-  color: red;
-}
-
-.right .edit-btn i:hover {
-  color: dodgerblue;
+h1 {
+  background-color: purple;
 }
 </style>
